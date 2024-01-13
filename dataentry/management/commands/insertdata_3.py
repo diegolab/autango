b3aa7ae04b1eeeb3f185cbefc353e8bda3423082
diego <diegodatapro@gmail.com>
2024-01-13 20:47:01 -0300

    InsertData Command - more options

---
+++ dataentry/management/commands/insertdata_3.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from dataentry.models import Student
+# ^ =======================================================|
+
+# todo -> I want to add some data to the database using the custom command
+
+class Command(BaseCommand):
+    help = "it will insert data to the database"
+
+    def handle(self, *args, **kwargs):
+        # logic goes here
+        dataset = [
+            {'roll_no': 1002, 'name': 'Lala', 'age':21},
+            {'roll_no': 1012, 'name': 'Asdasa', 'age':22},
+            {'roll_no': 1302, 'name': 'Varba', 'age':23}
+        ]
+        
+        for data in dataset:
+            
+            Student.objects.create(roll_no=data['roll_no'], name=data['name'], age=data['age'])
+        
+        self.stdout.write(self.style.SUCCESS('Data inserted successfully!'))
+
+            
+
