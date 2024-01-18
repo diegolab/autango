e592152131bec66a8da4fab4a2f2ed419b277bd0
diego <diegodatapro@gmail.com>
2024-01-17 21:30:24 -0300

    Import Data to Any Model

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -1,9 +1,10 @@
-from django.core.management.base import BaseCommand, CommandParser
-from dataentry.models import Student
+from django.core.management.base import BaseCommand, CommandError
+# from dataentry.models import Student
+from django.apps import apps
 import csv
 # ^ ==============================================================================|
 
-# * proposed command - python manage.py importdata <file_path>
+# * proposed command - python manage.py importdata <file_path> model name
 
 
 class Command(BaseCommand):
@@ -11,11 +12,27 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('file_path', type=str, help="Path to the CSV file")
+        parser.add_argument('model_name', type=str, help='Name of the model')
         
 
     def handle(self, *args, **kwargs):
         # logic goes here
         file_path = kwargs['file_path']
+        model_name = kwargs['model_name'].capitalize()
+
+        # Search for the model across all installed apps
+        model = None
+        for app_config in apps.get_app_configs():
+            ### try to search for the model inside the app
+            try:
+                model = apps.get_model(app_config.label, model_name)
+                break   # stop searching once the model is found
+            except LookupError:
+                continue    # model not found in this app..continue searching in next app.
+
+        if not model:
+            raise CommandError(f'Model "{model_name}" not found in any app!')
+
         
         with open(file_path, 'r') as file:
             reader = csv.DictReader(file)
@@ -24,7 +41,7 @@ class Command(BaseCommand):
             for row in reader:
                 # print(row)
                 
-                Student.objects.create(**row)
+                model.objects.create(**row)
 
 
         self.stdout.write(self.style.SUCCESS('Data imported from CSV successfully! '))
